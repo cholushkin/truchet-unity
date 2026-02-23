fd16637ee37914e221abc6c369e67952c201f6be
Max <cholushkin@gmail.com>
2026-02-23 20:34:35 +0200

    initial commit add: submodule ScriptUtils

---
+++ UserScriptsCoding/MakePromptDevTogetherPortToUnity.py
@@ -0,0 +1,13 @@
+import os, sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../ScriptUtils")))
+from Core.PromptContextCollector.PromptContextCollector import PromptContextCollector
+
+PromptContextCollector(
+    directories=["Unity/TruchetTiles/Core/Runtime/truchlib"],  # Wildcards and folders
+    includes=["*.cs", "*.md"],
+    files=[],
+    ignores=[],
+    template_path="UserScriptsCoding/TextTemplates/DevelopTogetherTemplate.txt",  # Relative to project root
+    template_vars={},
+    output_path="UserScriptsCoding/Outputs/PromptPortTruchetTiles.txt"
+).run()
